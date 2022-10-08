eabff6d883af578823d6854c06fcb5a01ba8fe39
gongcy <gongchengyao9797@126.com>
2022-10-08 10:43:51 +0800

    first commit

---
+++ src/main/java/com/yy/tomcat/ServletDemo.java
@@ -0,0 +1,13 @@
+package com.yy.tomcat;
+
+/**
+ * @author gongcy
+ * @date 2022/10/7 10:02 上午
+ * @Description
+ */
+public class ServletDemo{
+
+    public static void main(String[] args) {
+        System.out.println("测试生成提交的版本号 ");
+    }
+}
