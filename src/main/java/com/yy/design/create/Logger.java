7414cf4e4fe83fb7614f5b79d5a30d6420ad2968
gongcy <gongchengyao9797@126.com>
2022-10-17 20:50:34 +0800

    添加设计模式结构

--- src/main/java/com/yy/design/create/Logger.java
+++ src/main/java/com/yy/design/create/Logger.java
@@ -1,6 +1,26 @@
-package com.yy.design.create;/**
-  * @author gongcy
-  * @date 2022/10/17 8:37 下午
-  * @Description
-*/public class Logger {
+package com.yy.design.create;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author gongcy
+ * @date 2022/10/17 8:37 下午
+ * @Description
+ */
+public class Logger {
+
+    private FileWriter writer;
+
+    public Logger() throws IOException {
+        File file = new File("/Users/songguo77/log.txt");
+        writer = new FileWriter(file,true);
+    }
+
+    public void log(String message) throws IOException {
+        synchronized (Logger.class) {
+            writer.write(message);
+        }
+    }
 }
