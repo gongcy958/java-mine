53b452bfd4e102b5bd6d82d2bced80f51922239f
gongcy <gongchengyao9797@126.com>
2022-10-17 20:50:34 +0800

    conflict fix

--- src/main/java/com/yy/design/create/Logger.java
+++ src/main/java/com/yy/design/create/Logger.java
@@ -1,14 +1,30 @@
 
+
 package com.yy.design.create;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * @author gongcy
- * @date 2022/10/17 8:51 下午
+ * @date 2022/10/17 8:37 下午
  * @Description
  */
 public class Logger {
 
-    public Logger() {
+    private FileWriter writer;
+
+    public Logger() throws IOException {
+        File file = new File("/Users/songguo77/log.txt");
+        writer = new FileWriter(file,true);
+    }
+
+    public void log(String message) throws IOException {
+        synchronized (Logger.class) {
+            writer.write(message);
+        }
     }
 
 }
