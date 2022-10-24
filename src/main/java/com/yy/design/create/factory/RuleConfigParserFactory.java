6204c32866179092809eb0611ee0b8f0eeb70aac
gongcy <gongchengyao9797@126.com>
2022-10-24 16:23:18 +0800

    简单工厂+单例模式优化

--- src/main/java/com/yy/design/create/factory/RuleConfigParserFactory.java
+++ src/main/java/com/yy/design/create/factory/RuleConfigParserFactory.java
@@ -1,5 +1,8 @@
 package com.yy.design.create.factory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author gongcy
  * @date 2022/10/24 3:54 下午
@@ -7,18 +10,35 @@ package com.yy.design.create.factory;
  */
 public class RuleConfigParserFactory {
 
+    // 优化3 单例模式+简单工厂
+
+    private static final Map<String,IRuleConfigParser> cacheParsers = new HashMap<>();
+
+    static {
+        cacheParsers.put("json",new JsonRuleConfigParser());
+        cacheParsers.put("xml",new XmlRuleConfigParser());
+        cacheParsers.put("yaml",new YamlConfigRuleParser());
+        cacheParsers.put("properties",new PropertiesRuleConfigParser());
+    }
+
     public static IRuleConfigParser createParser(String configFormat) {
-        IRuleConfigParser parser = null;
-        if ("json".equalsIgnoreCase(configFormat)) {
-            parser = new JsonRuleConfigParser();
-        } else if ("xml".equalsIgnoreCase(configFormat)) {
-            parser = new XmlRuleConfigParser();
-        } else if ("yaml".equalsIgnoreCase(configFormat)) {
-            parser = new YamlConfigRuleParser();
-        } else if ("properties".equalsIgnoreCase(configFormat)) {
-            parser = new PropertiesRuleConfigParser();
+//        IRuleConfigParser parser = null;
+//        if ("json".equalsIgnoreCase(configFormat)) {
+//            parser = new JsonRuleConfigParser();
+//        } else if ("xml".equalsIgnoreCase(configFormat)) {
+//            parser = new XmlRuleConfigParser();
+//        } else if ("yaml".equalsIgnoreCase(configFormat)) {
+//            parser = new YamlConfigRuleParser();
+//        } else if ("properties".equalsIgnoreCase(configFormat)) {
+//            parser = new PropertiesRuleConfigParser();
+//        }
+//
+//        return parser;
+
+        if (configFormat == null || configFormat.isEmpty()) {
+            return null;
         }
 
-        return parser;
+        return cacheParsers.get(configFormat);
     }
 }
