ba54df302b4800e9e3531057b8607de4ddfac58b
gongcy <gongchengyao9797@126.com>
2023-01-10 15:18:44 +0800

    log info add.

---
+++ src/main/java/algorithm/doublepoint/log/txt.java
@@ -0,0 +1,12 @@
+package algorithm.doublepoint.log;
+
+/**
+ * @author gongcy
+ * @date 2023/1/10 3:18 下午
+ * @Description
+ */
+public class txt {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
