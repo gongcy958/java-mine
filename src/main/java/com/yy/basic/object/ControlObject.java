f923e674c6dd463602f4b105456fa033256771f8
gongcy <gongchengyao9797@126.com>
2023-01-11 12:49:51 +0800

    stash info

---
+++ src/main/java/com/yy/basic/object/ControlObject.java
@@ -0,0 +1,17 @@
+package com.yy.basic.object;
+
+/**
+ * @author gongcy
+ * @date 2022/10/19 10:04 上午
+ * @Description
+ */
+public class ControlObject {
+
+    public static void main(String[] args) {
+        String 宫成尧 = "我本人";
+        String mine = "我本人";
+
+        System.out.println(宫成尧);
+        System.out.println(mine);
+    }
+}
